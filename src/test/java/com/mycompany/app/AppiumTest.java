d45128bc8070aee680581589e8a429da350ccb6f
manish021085 <manishksingh0210@gmail.com>
2019-07-25 11:49:51 +0530

    First Commit , Existing Project

---
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeApAndroid()
+	{
+		System.out.println("NativeApAndroid");
+
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+
+	}
+
+}
